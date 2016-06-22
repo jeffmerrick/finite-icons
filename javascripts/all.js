55b513aa6726f3fe4a10bfa4f642435d11dff467
Jeff Merrick <jeff@doejo.com>
2016-06-22 13:26:25 -0500

    Site updated to 63e4e82

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $(".sizer").find("button").on("click", function(){
-    $(".glyphs").removeClass (function (index, css) {
+    $("body").removeClass (function (index, css) {
         return (css.match (/(^|\s)size-\S+/g) || []).join(' ');
     }).addClass("size-"+$(this).data("size"));
     $(".sizer").find("button").removeClass('active');
