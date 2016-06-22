b427aa9c36187303207a0d41283aabe854316d75
Jeff Merrick <jeff@doejo.com>
2016-06-22 12:43:45 -0500

    Site updated to 027951e

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,1 +1,9 @@
-// This is where it all goes :)
+$(document).ready(function(){
+  $(".sizer").find("button").on("click", function(){
+    $(".glyphs").removeClass (function (index, css) {
+        return (css.match (/(^|\s)size-\S+/g) || []).join(' ');
+    }).addClass("size-"+$(this).data("size"));
+    $(".sizer").find("button").removeClass('active');
+    $(this).addClass("active");
+  });
+});
